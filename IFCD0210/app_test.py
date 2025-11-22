46e8010168767d8328f88084fa15cd6fa85109c5
noemirtil <mirtil.noe@gmail.com>
2025-11-22 13:33:55 +0100

    Test Flask app prints psql data

--- IFCD0210/app_test.py
+++ IFCD0210/app_test.py
@@ -15,9 +15,14 @@ def index():
                                "brands"."name", "brands"."website" FROM "products"
                                 JOIN "brands" ON "brands"."id" = "products"."brand_id"
                                """
-        )
+        ).fetchall()
 
-    return results
+    return f"""
+    <H1>SOME RESULTS OF A QUERY:</H1>
+    <p>Product: {results[0][1]}, Brand: {results[0][2]}</p>
+    <p>Product: {results[1][1]}, Brand: {results[1][2]}</p>
+    <p>Product: {results[2][1]}, Brand: {results[2][2]}</p>
+"""
 
 
 if __name__ == "__main__":
