d4ab2212617b3f504ceb41230f80837af6db371f
noemirtil <mirtil.noe@gmail.com>
2025-11-28 11:47:11 +0100

    Added blueprint, route and template

--- app/routes/product_insert.py
+++ app/routes/product_insert.py
@@ -2,50 +2,19 @@ from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
 from app.db.connection import get_db
 
-product_insert_bp = Blueprint("main", __name__)
+product_insert_bp = Blueprint("product_insert", __name__)
 
 
 @product_insert_bp.route("/product_insert")
 def product_insert():
     db = get_db()
     cur = db.cursor()
+    products = cur.execute(get_products()).fetchall()
 
-    page = request.args.get("page", 1, type=int)
-    per_page = 18
-    offset = (page - 1) * per_page
-
-    if request.args.get("query") == "get_protein_score()":
-        cur.execute(
-            get_protein_score(),
-            (per_page, offset),
-        )
-    products = cur.fetchall()
-
-    processed_products = []
-    for product_row in products:
-        product = dict(product_row)  # Convert row to dictionary if needed
-
-        letter, color = _assign_nutriscore_data(product["nutr_score_fr"])
-
-        product["nutri_letter"] = letter
-        product["nutri_color"] = color
-
-        processed_products.append(product)
-
-    cur.execute("SELECT COUNT(*) as count FROM products")
-    total_products = cur.fetchone()["count"]
-    total_pages = (total_products + per_page - 1) // per_page
-
-    return render_template(
-        "main/product_insert.html",
-        products=products,
-        page=page,
-        total_pages=total_pages,
-    )
+    return render_template("product_insert/product_insert.html", products=products)
 
 
-def get_protein_score():
-    # To look for the highest Protein Score cost-effective products
+def get_products():
     query = """
     SELECT
         products.id,
@@ -68,9 +37,7 @@ def get_protein_score():
         prices."date" AS price_date,
         stores."name" AS store_name,
         countries.country,
-        currencies.currency_code,
-        CAST((products.protein / GREATEST(SUM(prices.weight * prices.price), 0.01))
-        / GREATEST(products.fat, 0.001) * 10000000 AS INT) AS protein_score
+        currencies.currency_code
     FROM products
     LEFT JOIN prices ON prices.product_id = products.id
     LEFT JOIN brands ON products.brand_id = brands.id
@@ -98,7 +65,6 @@ def get_protein_score():
         stores."name",
         countries.country,
         currencies.currency_code
-    ORDER BY protein_score DESC NULLS LAST
-    LIMIT %s OFFSET %s
+    ORDER BY products.name
         """
     return query
